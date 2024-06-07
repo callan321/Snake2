89a247143f0cffb36b2b97b5d3515617fa641a91
callan321 <callan.mitchell11@gmail.com>
2024-06-08 03:05:13 +1000

    cleaned more code

--- interfaces/play_game.py
+++ interfaces/play_game.py
@@ -34,24 +34,24 @@ class PlayGame:
         """Run the main game loop."""
         while self.running:
             self.clock.tick(self.config.FPS)
-            current_time = pygame.time.get_ticks()
-            if not self.paused and current_time - self.last_update_time > 1000 // self.speed:
-                self.last_update_time = current_time
-                self.logic.update()
-
+            self.handle_time()
             self.event_handler.handle_events()
-
+            self.update_game_elements()
+            self.draw()
             if self.return_to_menu:
                 return self.config.MENU
 
-            self.update_game_elements()
-            self.draw()
+    def handle_time(self) -> None:
+        """Handle the game timing and updates based on game speed."""
+        current_time = pygame.time.get_ticks()
+        if not self.paused and current_time - self.last_update_time > 1000 // self.speed:
+            self.last_update_time = current_time
+            self.logic.update()
 
     def update_game_elements(self) -> None:
         """Update game elements such as UI and renderer dimensions."""
         self.ui.update_dimensions()
         self.renderer.update_screen_size()
-
 
     def draw(self) -> None:
         """Draw the game elements."""
