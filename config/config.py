447e7bfd6e7314e6c5917f6325e4716e739f2a7c
callan321 <callan.mitchell11@gmail.com>
2024-06-09 15:23:06 +1000

    resize now working as intented fixed bugs

--- config/config.py
+++ config/config.py
@@ -5,12 +5,12 @@ class GameConfig:
     def __init__(self, screen_width, screen_height, settings_file='config/settings.json'):
         self.settings_file = settings_file
         self.settings = self.load_settings(settings_file)
-        self.base_width = self.settings['base_sacles']['SCREEN_WIDTH']
-        self.base_height = self.settings['base_sacles']['SCREEN_HEIGHT']
+        self.BASE_SCREEN_WIDTH = self.settings['base_sacles']['SCREEN_WIDTH']
+        self.BASE_SCREEN_HEIGHT = self.settings['base_sacles']['SCREEN_HEIGHT']
         self.update_config(screen_width, screen_height)
         self.number_of_cells = self.settings['game_settings'].get("number_of_cells", 20)
-        self.snake_size = self.settings['game_settings'].get("snake_size", 1)
-        self.game_speed = self.settings['game_settings'].get("game_speed", 10)
+        self.snake_size = self.settings['game_settings'].get("snake_size", 2)
+        self.game_speed = self.settings['game_settings'].get("game_speed", 1)
 
     def load_settings(self, file_path):
         if not os.path.exists(file_path):
@@ -28,8 +28,8 @@ class GameConfig:
 
     
     def scale_value(self, base_value, new_screen_width, new_screen_height):
-        width_scale = new_screen_width / self.screen_width
-        height_scale = new_screen_height / self.screen_height
+        width_scale = new_screen_width / self.BASE_SCREEN_WIDTH
+        height_scale = new_screen_height / self.BASE_SCREEN_HEIGHT
         
         min_scale = min(width_scale, height_scale)
         
@@ -37,41 +37,40 @@ class GameConfig:
         return int(scaled_value)
     
     def update_config(self, screen_width, screen_height):
-        self.screen_width = screen_width
-        self.screen_height = screen_height
+        game_scales = self.settings['game_scales']
 
         # GAME SCALE CONFIG
         game_scales = self.settings['game_scales']
-        self.BORDER_THICKNESS = self.scale_value(game_scales['BORDER_THICKNESS'], self.base_width, self.screen_width)
-        self.GAME_WIDTH = self.scale_value(game_scales['GAME_WIDTH'], self.base_width, self.screen_width)
-        self.GAME_HEIGHT = self.scale_value(game_scales['GAME_HEIGHT'], self.base_height, self.screen_height)
+        self.BORDER_THICKNESS = self.scale_value(game_scales['BORDER_THICKNESS'], screen_width, screen_height)
+        self.GAME_WIDTH = self.scale_value(game_scales['GAME_WIDTH'], screen_width, screen_height)
+        self.GAME_HEIGHT = self.scale_value(game_scales['GAME_HEIGHT'], screen_width, screen_height)
         
         # speed button
         speed_button = game_scales['speed_button']
-        self.SB_WIDTH = self.scale_value(speed_button['SB_WIDTH'], self.base_width, self.screen_width)
-        self.SB_HEIGHT = self.scale_value(speed_button['SB_HEIGHT'], self.base_height, self.screen_height)
-        self.SB_BORDER_RADIUS = self.scale_value(speed_button['SB_BORDER_RADIUS'], self.base_width, self.screen_width)
-        self.SB_FONT_SIZE = self.scale_value(speed_button['SB_FONT_SIZE'], self.base_height, self.screen_height)
+        self.SB_WIDTH = self.scale_value(speed_button['SB_WIDTH'], screen_width, screen_height)
+        self.SB_HEIGHT = self.scale_value(speed_button['SB_HEIGHT'], screen_width, screen_height)
+        self.SB_BORDER_RADIUS = self.scale_value(speed_button['SB_BORDER_RADIUS'], screen_width, screen_height)
+        self.SB_FONT_SIZE = self.scale_value(speed_button['SB_FONT_SIZE'], screen_width, screen_height)
         
         # MENU SCALES
         menu_scales = self.settings['menu_scales']
         self.TITLE_Y_OFFSET_MULTIPLIER = menu_scales['TITLE_Y_OFFSET_MULTIPLIER']
-        self.TITLE_FONT_SIZE = self.scale_value(menu_scales['TITLE_FONT_SIZE'], self.base_height, self.screen_height)
-        self.MB_Y_OFFSET = self.scale_value(menu_scales['MB_Y_OFFSET'], self.base_height, self.screen_height)
+        self.TITLE_FONT_SIZE = self.scale_value(menu_scales['TITLE_FONT_SIZE'], screen_width, screen_height)
+        self.MB_Y_OFFSET = self.scale_value(menu_scales['MB_Y_OFFSET'], screen_width, screen_height)
         
         # menu button
         menu_buttons = menu_scales['menu_button']
-        self.MB_WIDTH = self.scale_value(menu_buttons['MB_WIDTH'], self.base_width, self.screen_width)
-        self.MB_HEIGHT = self.scale_value(menu_buttons['MB_HEIGHT'], self.base_height, self.screen_height)
-        self.MB_BORDER_RADIUS = self.scale_value(menu_buttons['MB_BORDER_RADIUS'], self.base_width, self.screen_width)
-        self.BUTTON_FONT_SIZE = self.scale_value(menu_buttons['MB_FONT_SIZE'], self.base_height, self.screen_height)
+        self.MB_WIDTH = self.scale_value(menu_buttons['MB_WIDTH'], screen_width, screen_height)
+        self.MB_HEIGHT = self.scale_value(menu_buttons['MB_HEIGHT'], screen_width, screen_height)
+        self.MB_BORDER_RADIUS = self.scale_value(menu_buttons['MB_BORDER_RADIUS'], screen_width, screen_height)
+        self.BUTTON_FONT_SIZE = self.scale_value(menu_buttons['MB_FONT_SIZE'], screen_width, screen_height)
         
-        # size buttons
+        # game size buttons
         game_size_buttons = menu_scales['game_size_button']
-        self.GS_FONT_SIZE = self.scale_value(game_size_buttons['GS_FONT_SIZE'], self.base_height, self.screen_height)
-        self.GS_BUTTON_HEIGHT = self.scale_value(game_size_buttons['GS_BUTTON_HEIGHT'], self.base_height, self.screen_height)
-        self.GS_BUTTON_WIDTH = self.scale_value(game_size_buttons['GS_BUTTON_WIDTH'], self.base_width, self.screen_width)
-        self.GS_BORDER_RADIUS = self.scale_value(game_size_buttons['GS_BORDER_RADIUS'], self.base_width, self.screen_width)
+        self.GS_FONT_SIZE = self.scale_value(game_size_buttons['GS_FONT_SIZE'], screen_width, screen_height)
+        self.GS_BUTTON_HEIGHT = self.scale_value(game_size_buttons['GS_BUTTON_HEIGHT'], screen_width, screen_height)
+        self.GS_BUTTON_WIDTH = self.scale_value(game_size_buttons['GS_BUTTON_WIDTH'], screen_width, screen_height)
+        self.GS_BORDER_RADIUS = self.scale_value(game_size_buttons['GS_BORDER_RADIUS'], screen_width, screen_height)
 
 
         self.GAME_SIZE_BUTTONS = game_size_buttons['buttons']
